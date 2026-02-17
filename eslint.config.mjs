f68da423697dfdb3bfbc177e3e64ac196f35f986
Rafael Drigo <rafaeldrigo.rds@gmail.com>
2026-02-17 19:53:39 -0300

    feat: init

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginReactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig({
+  files: ['**/*.{mjs,ts,tsx}'],
+  ignores: ['dist/**', 'node_modules/**'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    eslintPluginReactHooks.configs.flat.recommended,
+    eslintPluginReactRefresh.configs.vite,
+    eslintConfigPrettier,
+  ],
+  plugins: {
+    prettier: eslintPluginPrettier,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+});
