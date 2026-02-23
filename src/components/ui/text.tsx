3937dfb11ba6076f6e0dbfe4049e276d2ffcdc06
Rafael Cristian Drigo <rafael.drigo@agropecuariabressiani.com.br>
2026-02-23 08:53:51 -0300

    feat(ui): add Text component

---
+++ src/components/ui/text.tsx
@@ -0,0 +1,58 @@
+import { cn } from '@/utils/class-name';
+import { Slot } from 'radix-ui';
+import React from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+export const textVariants = tv({
+  variants: {
+    variant: {
+      title: 'text-4xl sm:text-5xl md:text-7xl tracking-tight',
+      subtitle: 'text-3xl md:text-4xl',
+      paragraph: 'text-base leading-relaxed',
+      lead: 'text-lg md:text-xl',
+    },
+    weight: {
+      normal: 'font-normal',
+      medium: 'font-medium',
+      semibold: 'font-semibold',
+      bold: 'font-bold',
+    },
+    align: {
+      left: 'text-left',
+      center: 'text-center',
+      right: 'text-right',
+    },
+  },
+  defaultVariants: {
+    variant: 'paragraph',
+    weight: 'normal',
+    align: 'left',
+  },
+});
+
+export type TextProps = React.ComponentPropsWithoutRef<'p'> &
+  VariantProps<typeof textVariants> & {
+    as?: 'h1' | 'h2' | 'p';
+    asChild?: boolean;
+  };
+
+export function Text({
+  className,
+  align,
+  variant,
+  weight,
+  as = 'p',
+  asChild = false,
+  ...props
+}: TextProps): React.JSX.Element {
+  const Component = asChild ? Slot.Root : as;
+
+  return (
+    <Component
+      data-slot="text"
+      data-variant={variant}
+      className={cn(textVariants({ align, variant, weight, className }))}
+      {...props}
+    />
+  );
+}
